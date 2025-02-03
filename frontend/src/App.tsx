840c12da177baa278652071c135ab2d7ec60b033
Jacques Lucke <mail@jlucke.com>
2025-02-03 22:08:26 +0100

    initial embedded frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return "Hello World!";
+  return "Hello You!";
 }
 
 export default App;
--- src/start_server.rs
+++ src/start_server.rs
@@ -1,18 +1,31 @@
 use actix_web::http::header::{CacheControl, CacheDirective};
 use actix_web::middleware::DefaultHeaders;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
-#[actix_web::get("/")]
-async fn route_index() -> &'static str {
-    "Hey there!"
+static FRONTEND_FILES: include_dir::Dir = include_dir::include_dir!("./frontend/dist");
+
+#[actix_web::get("/{filename:.*}")]
+async fn route_frontend(req: actix_web::HttpRequest) -> impl Responder {
+    let path = req.match_info().get("filename");
+    let path = path.unwrap_or("not_found.html");
+    let path = if path.is_empty() { "index.html" } else { path };
+
+    if let Some(file) = FRONTEND_FILES.get_file(path) {
+        let mime_type = mime_guess::from_path(path).first_or_octet_stream();
+        HttpResponse::Ok()
+            .content_type(mime_type.to_string())
+            .body(file.contents())
+    } else {
+        HttpResponse::NotFound().body("File not found")
+    }
 }
 
 pub async fn start_server(listener: TcpListener) -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(DefaultHeaders::new().add(CacheControl(vec![CacheDirective::NoCache])))
-            .service(route_index)
+            .service(route_frontend)
     })
     .workers(1)
     .listen(listener)
