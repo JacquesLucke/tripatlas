56f626b7da309dc6a09f33ea1af13381f1e7b550
Jacques Lucke <mail@jlucke.com>
2025-02-07 22:23:33 +0100

    further simplification

--- libs/indexed_gtfs/src/lib.rs
+++ libs/indexed_gtfs/src/lib.rs
@@ -1,18 +1,8 @@
 mod structures;
 
-pub use structures::*;
+use std::path::Path;
 
-pub struct GtfsBuffers<'a> {
-    pub stop_times: Option<&'a [u8]>,
-    pub stops: Option<&'a [u8]>,
-    pub trips: Option<&'a [u8]>,
-    pub routes: Option<&'a [u8]>,
-    pub calendar: Option<&'a [u8]>,
-    pub calendar_dates: Option<&'a [u8]>,
-    pub agencies: Option<&'a [u8]>,
-    pub feed_infos: Option<&'a [u8]>,
-    pub attributions: Option<&'a [u8]>,
-}
+pub use structures::*;
 
 impl<'a> Gtfs<'a> {
     pub fn from_buffers(buffers: GtfsBuffers<'a>) -> std::result::Result<Self, ()> {
@@ -43,5 +33,107 @@ impl<'a> Gtfs<'a> {
             feed_infos: do_parse!(feed_infos, FeedInfos),
             attributions: do_parse!(attributions, Attributions),
         })
+    }
+}
+
+pub struct GtfsBuffers<'a> {
+    pub stop_times: Option<&'a [u8]>,
+    pub stops: Option<&'a [u8]>,
+    pub trips: Option<&'a [u8]>,
+    pub routes: Option<&'a [u8]>,
+    pub calendar: Option<&'a [u8]>,
+    pub calendar_dates: Option<&'a [u8]>,
+    pub agencies: Option<&'a [u8]>,
+    pub feed_infos: Option<&'a [u8]>,
+    pub attributions: Option<&'a [u8]>,
+}
+
+pub struct GtfsBuffersRAM {
+    pub stop_times: Option<Vec<u8>>,
+    pub stops: Option<Vec<u8>>,
+    pub trips: Option<Vec<u8>>,
+    pub routes: Option<Vec<u8>>,
+    pub calendar: Option<Vec<u8>>,
+    pub calendar_dates: Option<Vec<u8>>,
+    pub agencies: Option<Vec<u8>>,
+    pub feed_infos: Option<Vec<u8>>,
+    pub attributions: Option<Vec<u8>>,
+}
+
+impl GtfsBuffersRAM {
+    pub fn from_dir(gtfs_dir: &Path) -> Self {
+        Self {
+            stop_times: std::fs::read(gtfs_dir.join("stop_times.txt")).ok(),
+            stops: std::fs::read(gtfs_dir.join("stops.txt")).ok(),
+            trips: std::fs::read(gtfs_dir.join("trips.txt")).ok(),
+            routes: std::fs::read(gtfs_dir.join("routes.txt")).ok(),
+            calendar: std::fs::read(gtfs_dir.join("calendar.txt")).ok(),
+            calendar_dates: std::fs::read(gtfs_dir.join("calendar_dates.txt")).ok(),
+            agencies: std::fs::read(gtfs_dir.join("agency.txt")).ok(),
+            feed_infos: std::fs::read(gtfs_dir.join("feed_info.txt")).ok(),
+            attributions: std::fs::read(gtfs_dir.join("attributions.txt")).ok(),
+        }
+    }
+
+    pub fn to_slices<'a>(&'a self) -> GtfsBuffers<'a> {
+        GtfsBuffers {
+            stop_times: self.stop_times.as_ref().map(|s| &s[..]),
+            stops: self.stops.as_ref().map(|s| &s[..]),
+            trips: self.trips.as_ref().map(|s| &s[..]),
+            routes: self.routes.as_ref().map(|s| &s[..]),
+            calendar: self.calendar.as_ref().map(|s| &s[..]),
+            calendar_dates: self.calendar_dates.as_ref().map(|s| &s[..]),
+            agencies: self.agencies.as_ref().map(|s| &s[..]),
+            feed_infos: self.feed_infos.as_ref().map(|s| &s[..]),
+            attributions: self.attributions.as_ref().map(|s| &s[..]),
+        }
+    }
+}
+
+pub struct GtfsBuffersMmap {
+    pub stop_times: Option<memmap2::Mmap>,
+    pub stops: Option<memmap2::Mmap>,
+    pub trips: Option<memmap2::Mmap>,
+    pub routes: Option<memmap2::Mmap>,
+    pub calendar: Option<memmap2::Mmap>,
+    pub calendar_dates: Option<memmap2::Mmap>,
+    pub agencies: Option<memmap2::Mmap>,
+    pub feed_infos: Option<memmap2::Mmap>,
+    pub attributions: Option<memmap2::Mmap>,
+}
+
+impl GtfsBuffersMmap {
+    pub unsafe fn from_dir(gtfs_dir: &Path) -> Self {
+        Self {
+            stop_times: Self::load(gtfs_dir, "stop_times.txt"),
+            stops: Self::load(gtfs_dir, "stops.txt"),
+            trips: Self::load(gtfs_dir, "trips.txt"),
+            routes: Self::load(gtfs_dir, "routes.txt"),
+            calendar: Self::load(gtfs_dir, "calendar.txt"),
+            calendar_dates: Self::load(gtfs_dir, "calendar_dates.txt"),
+            agencies: Self::load(gtfs_dir, "agency.txt"),
+            feed_infos: Self::load(gtfs_dir, "feed_info.txt"),
+            attributions: Self::load(gtfs_dir, "attributions.txt"),
+        }
+    }
+
+    unsafe fn load(gtfs_dir: &Path, file_name: &str) -> Option<memmap2::Mmap> {
+        memmap2::Mmap::map(&std::fs::File::open(gtfs_dir.join(file_name)).unwrap()).ok()
+    }
+}
+
+impl GtfsBuffersMmap {
+    pub fn to_slices<'a>(&'a self) -> GtfsBuffers<'a> {
+        GtfsBuffers {
+            stop_times: self.stop_times.as_ref().map(|s| &s[..]),
+            stops: self.stops.as_ref().map(|s| &s[..]),
+            trips: self.trips.as_ref().map(|s| &s[..]),
+            routes: self.routes.as_ref().map(|s| &s[..]),
+            calendar: self.calendar.as_ref().map(|s| &s[..]),
+            calendar_dates: self.calendar_dates.as_ref().map(|s| &s[..]),
+            agencies: self.agencies.as_ref().map(|s| &s[..]),
+            feed_infos: self.feed_infos.as_ref().map(|s| &s[..]),
+            attributions: self.attributions.as_ref().map(|s| &s[..]),
+        }
     }
 }
--- tripatlas/src/indexed_gtfs.rs
+++ tripatlas/src/indexed_gtfs.rs
@@ -3,48 +3,12 @@ use std::{path::Path, time::Instant};
 pub fn parse_performance_test() {
     let gtfs_dir = Path::new("/home/jacques/Documents/gtfs_germany");
 
-    let stop_times_path = gtfs_dir.join("stop_times.txt");
-    let stops_path = gtfs_dir.join("stops.txt");
-    let trips_path = gtfs_dir.join("trips.txt");
-    let routes_path = gtfs_dir.join("routes.txt");
-    let calendar_path = gtfs_dir.join("calendar.txt");
-    let calendar_dates_path = gtfs_dir.join("calendar_dates.txt");
-    let agencies_path = gtfs_dir.join("agency.txt");
-    let feed_infos_path = gtfs_dir.join("feed_info.txt");
-    let attributions_path = gtfs_dir.join("attributions.txt");
-
     let start_time = Instant::now();
 
-    let stop_times_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(stop_times_path).unwrap()) }.unwrap();
-    let stops_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(stops_path).unwrap()) }.unwrap();
-    let trips_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(trips_path).unwrap()) }.unwrap();
-    let routes_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(routes_path).unwrap()) }.unwrap();
-    let calendar_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(calendar_path).unwrap()) }.unwrap();
-    let calendar_dates_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(calendar_dates_path).unwrap()) }.unwrap();
-    let agencies_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(agencies_path).unwrap()) }.unwrap();
-    let feed_infos_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(feed_infos_path).unwrap()) }.unwrap();
-    let attributions_mmap =
-        unsafe { memmap2::Mmap::map(&std::fs::File::open(attributions_path).unwrap()) }.unwrap();
+    // let buffers_ram = indexed_gtfs::GtfsBuffersRAM::from_dir(&gtfs_dir);
+    let buffers_mmap = unsafe { indexed_gtfs::GtfsBuffersMmap::from_dir(&gtfs_dir) };
 
-    let gtfs = indexed_gtfs::Gtfs::from_buffers(indexed_gtfs::GtfsBuffers {
-        stop_times: Some(&stop_times_mmap[..]),
-        stops: Some(&stops_mmap[..]),
-        trips: Some(&trips_mmap[..]),
-        routes: Some(&routes_mmap[..]),
-        calendar: Some(&calendar_mmap[..]),
-        calendar_dates: Some(&calendar_dates_mmap[..]),
-        agencies: Some(&agencies_mmap[..]),
-        feed_infos: Some(&feed_infos_mmap[..]),
-        attributions: Some(&attributions_mmap[..]),
-    });
+    let gtfs = indexed_gtfs::Gtfs::from_buffers(buffers_mmap.to_slices());
 
     println!("Time elapsed: {:?}", start_time.elapsed());
 
