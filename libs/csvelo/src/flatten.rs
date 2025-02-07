812776542024256e2ac26ab5fd1b2fa0c3a49421
Jacques Lucke <mail@jlucke.com>
2025-02-07 19:56:27 +0100

    cleanup

--- libs/csvelo/src/flatten.rs
+++ libs/csvelo/src/flatten.rs
@@ -1,6 +1,9 @@
 use rayon::prelude::*;
 
+/// Concatenates the given slices into a single vector.
 pub fn flatten_slices<T: Clone + Send + Sync>(slices: &[&[T]]) -> Vec<T> {
+    // Compute offsets and total size of the output so that we can reserve the right
+    // amount of memory and then copy the data in parallel.
     let mut offsets = vec![];
     let mut start = 0;
     offsets.push(start);
@@ -10,9 +13,13 @@ pub fn flatten_slices<T: Clone + Send + Sync>(slices: &[&[T]]) -> Vec<T> {
     }
     let total_len = start;
 
+    // Allocate the output vector.
     let mut flattened: Vec<T> = Vec::with_capacity(total_len);
+    // This is safe as long because all the elements are initialized below.
     unsafe { flattened.set_len(total_len) };
     let mut flattened_slice = flattened.as_mut_slice();
+
+    // Split the output vector into slices for each chunk.
     let mut dst_slices = vec![];
     for slice in slices {
         let (left, right) = flattened_slice.split_at_mut(slice.len());
@@ -20,9 +27,15 @@ pub fn flatten_slices<T: Clone + Send + Sync>(slices: &[&[T]]) -> Vec<T> {
         flattened_slice = right;
     }
 
-    slices.par_iter().zip(dst_slices).for_each(|(src, dst)| {
-        dst.clone_from_slice(src);
-    });
+    // Actually copy over the data.
+    slices
+        .par_iter()
+        .zip(dst_slices)
+        .for_each(|(src, dst)| unsafe {
+            for i in 0..src.len() {
+                std::ptr::write(dst.get_unchecked_mut(i), src.get_unchecked(i).clone());
+            }
+        });
 
     flattened
 }
