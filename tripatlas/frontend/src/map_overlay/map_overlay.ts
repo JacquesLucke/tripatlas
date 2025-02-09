3f0fe84885ec9a9ca3c3950f08d1248e45ef0e8d
Jacques Lucke <mail@jlucke.com>
2025-02-09 13:33:32 +0100

    test tile detection

--- tripatlas/frontend/src/map_overlay/map_overlay.ts
+++ tripatlas/frontend/src/map_overlay/map_overlay.ts
@@ -6,7 +6,7 @@ let overlayContainer = document.getElementById(
 
 export class MapOverlay {
   canvas: HTMLCanvasElement;
-  gl: WebGL2RenderingContext;
+  ctx: CanvasRenderingContext2D;
   map: L.Map;
 
   constructor(map: L.Map) {
@@ -19,12 +19,12 @@ export class MapOverlay {
     this.canvas.style.width = "100%";
     this.canvas.style.pointerEvents = "none";
     overlayContainer.appendChild(this.canvas);
-    const gl = this.canvas.getContext("webgl2");
-    if (!gl) {
-      throw new Error("WebGL2 not supported");
+    const ctx = this.canvas.getContext("2d");
+    if (!ctx) {
+      throw new Error("Canvas drawing not supported");
     }
-    this.gl = gl;
-    gl.getExtension("OES_texture_float_linear");
+    this.ctx = ctx;
+    // gl.getExtension("OES_texture_float_linear");
 
     setTimeout(() => {
       this.render();
@@ -32,15 +32,94 @@ export class MapOverlay {
   }
 
   render() {
+    const ctx = this.ctx;
     const map = this.map;
     const mapSize = map.getSize();
 
     this.canvas.width = mapSize.x;
     this.canvas.height = mapSize.y;
 
-    const gl = this.gl;
-    gl.viewport(0, 0, mapSize.x, mapSize.y);
-    gl.clearColor(0, 0, 0, 0.5);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    let canvasTiles = getCanvasTiles(map, 8, -100);
+
+    ctx.clearRect(0, 0, mapSize.x, mapSize.y);
+    ctx.strokeStyle = "#f00";
+    ctx.lineWidth = 1;
+    for (const canvasTile of canvasTiles) {
+      ctx.beginPath();
+      ctx.moveTo(canvasTile.left, canvasTile.top);
+      ctx.lineTo(canvasTile.right, canvasTile.top);
+      ctx.lineTo(canvasTile.right, canvasTile.bottom);
+      ctx.lineTo(canvasTile.left, canvasTile.bottom);
+      ctx.lineTo(canvasTile.left, canvasTile.top);
+      ctx.stroke();
+    }
   }
+}
+
+interface CanvasTileInfo {
+  tileX: number;
+  tileY: number;
+  zoom: number;
+  latLngBounds: L.LatLngBounds;
+
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+}
+
+function getCanvasTiles(map: L.Map, tileZoomLevel: number, paddingPx: number) {
+  const mapZoom = map.getZoom();
+  const mapBounds = map.getBounds();
+  const mapPixelBounds = map.getPixelBounds();
+  const mapTopLeftPx = mapPixelBounds.getTopLeft();
+  const crs = map.options.crs!;
+  const tileSizePx = 256;
+
+  const actualPaddingPx = paddingPx * Math.pow(2, tileZoomLevel - mapZoom);
+  console.log(actualPaddingPx);
+
+  const tileMin = crs
+    .latLngToPoint(mapBounds.getNorthWest(), tileZoomLevel)
+    .subtract([actualPaddingPx, actualPaddingPx])
+    .divideBy(tileSizePx)
+    .floor();
+  const tileMax = crs
+    .latLngToPoint(mapBounds.getSouthEast(), tileZoomLevel)
+    .add([actualPaddingPx, actualPaddingPx])
+    .divideBy(tileSizePx)
+    .floor();
+
+  let canvasTiles: CanvasTileInfo[] = [];
+  for (let tileX = tileMin.x; tileX <= tileMax.x; tileX++) {
+    for (let tileY = tileMin.y; tileY <= tileMax.y; tileY++) {
+      const tileMinLatLng = crs.pointToLatLng(
+        new L.Point(tileX, tileY).multiplyBy(tileSizePx),
+        tileZoomLevel
+      );
+      const tileMaxLatLng = crs.pointToLatLng(
+        new L.Point(tileX + 1, tileY + 1).multiplyBy(tileSizePx),
+        tileZoomLevel
+      );
+      const tileMinCanvasPx = crs
+        .latLngToPoint(tileMinLatLng, mapZoom)
+        .subtract(mapTopLeftPx);
+      const tileMaxCanvasPx = crs
+        .latLngToPoint(tileMaxLatLng, mapZoom)
+        .subtract(mapTopLeftPx);
+
+      canvasTiles.push({
+        tileX: tileX,
+        tileY: tileY,
+        zoom: tileZoomLevel,
+        latLngBounds: new L.LatLngBounds([tileMinLatLng, tileMaxLatLng]),
+        left: tileMinCanvasPx.x,
+        right: tileMaxCanvasPx.x,
+        top: tileMinCanvasPx.y,
+        bottom: tileMaxCanvasPx.y,
+      });
+    }
+  }
+
+  return canvasTiles;
 }
