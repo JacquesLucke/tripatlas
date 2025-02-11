73005353ad6dfa20bd7bfa0e445e449da1b2c47e
Jacques Lucke <mail@jlucke.com>
2025-02-11 23:41:41 +0100

    extract more routes into separate files

---
+++ tripatlas/src/routes/mod.rs
@@ -0,0 +1,2 @@
+pub mod api_basics;
+pub mod frontend;
--- tripatlas/src/start_server.rs
+++ tripatlas/src/start_server.rs
@@ -26,20 +26,6 @@ pub struct Config {
     pub allow_shutdown_from_frontend: bool,
 }
 
-#[actix_web::get("/api")]
-async fn route_api_root(state: web::Data<State>) -> impl Responder {
-    state.metrics.api_root_requests_total.inc();
-    HttpResponse::Ok().body("The api is working.")
-}
-
-#[actix_web::get("/api/config")]
-async fn route_api_config(state: web::Data<State>) -> impl Responder {
-    state.metrics.config_requests_total.inc();
-    HttpResponse::Ok().json(Config {
-        allow_shutdown_from_frontend: state.config.allow_shutdown_from_frontend,
-    })
-}
-
 #[actix_web::get("/api/some_hash_23424/{zoom}_{tile_x}_{tile_y}.bin")]
 async fn route_api_tile_color(
     state: web::Data<State>,
@@ -60,30 +46,6 @@ async fn route_api_tile_color(
     HttpResponse::Ok()
         .insert_header(("Cache-Control", "public, max-age=31536000, immutable"))
         .body(color)
-}
-
-#[actix_web::post("/api/shutdown")]
-async fn route_api_shutdown(state: web::Data<State>) -> impl Responder {
-    if !state.config.allow_shutdown_from_frontend {
-        return HttpResponse::Unauthorized().body("Shutdown is not allowed.");
-    }
-    tokio::task::spawn(async {
-        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-        std::process::exit(0);
-    });
-    HttpResponse::Ok().body("Shutting down server.")
-}
-
-#[actix_web::get("/api/metrics")]
-async fn route_api_metrics(state: web::Data<State>) -> impl Responder {
-    state.metrics.metrics_requests_total.inc();
-    let encoder = prometheus::TextEncoder::new();
-    let metric_families = state.metrics.registry.gather();
-    let mut buffer = vec![];
-    prometheus::Encoder::encode(&encoder, &metric_families, &mut buffer).unwrap();
-    HttpResponse::Ok()
-        .content_type(prometheus::Encoder::format_type(&encoder))
-        .body(buffer)
 }
 
 fn prepare_prometheus_metrics() -> PrometheusMetrics {
@@ -178,10 +140,10 @@ pub async fn start_server(
         App::new()
             .app_data(state.clone())
             .wrap(Cors::permissive())
-            .service(route_api_root)
-            .service(route_api_config)
-            .service(route_api_shutdown)
-            .service(route_api_metrics)
+            .service(crate::routes::api_basics::route_api_root)
+            .service(crate::routes::api_basics::route_api_config)
+            .service(crate::routes::api_basics::route_api_shutdown)
+            .service(crate::routes::api_basics::route_api_metrics)
             .service(route_api_tile_color)
             .service(crate::routes::frontend::route_frontend)
     })
